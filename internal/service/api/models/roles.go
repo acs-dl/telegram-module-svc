e33bb9fce41ad32c81611aede272d06604b91ea1
Maksym Hrynenko <mg06062003@gmail.com>
2023-05-18 16:36:13 +0300

    return chats in get availdable roles

--- internal/service/api/models/roles.go
+++ internal/service/api/models/roles.go
@@ -8,7 +8,7 @@ var roles = []resources.AccessLevel{
 	{Name: "Member", Value: "member"},
 }
 
-func NewRolesModel(found bool, roles []resources.AccessLevel) resources.Roles {
+func NewRolesModel(found bool, roles []resources.AccessLevel, chats []resources.Chat) resources.Roles {
 	result := resources.Roles{
 		Key: resources.Key{
 			ID:   "roles",
@@ -17,14 +17,15 @@ func NewRolesModel(found bool, roles []resources.AccessLevel) resources.Roles {
 		Attributes: resources.RolesAttributes{
 			Req:   found,
 			Roles: roles,
+			Chats: chats,
 		},
 	}
 
 	return result
 }
 
-func NewRolesResponse(found bool) resources.RolesResponse {
+func NewRolesResponse(found bool, chats []resources.Chat) resources.RolesResponse {
 	return resources.RolesResponse{
-		Data: NewRolesModel(found, roles),
+		Data: NewRolesModel(found, roles, chats),
 	}
 }
--- resources/model_roles_attributes.go
+++ resources/model_roles_attributes.go
@@ -5,6 +5,7 @@
 package resources
 
 type RolesAttributes struct {
+	Chats []Chat `json:"chats"`
 	// flag to show if any role was found for given link
 	Req   bool          `json:"req"`
 	Roles []AccessLevel `json:"roles"`
